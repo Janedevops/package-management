55b647e48d2ec3c781f1bc33815ad95ebf45b637
Janedevops <111403093+Janedevops@users.noreply.github.com>
2022-09-17 00:22:56 -0600

    Update Jenkins-redhat8-install28.sh

--- JENKINS-INSTALLATION/Jenkins-redhat8-install28.sh
+++ JENKINS-INSTALLATION/Jenkins-redhat8-install28.sh
@@ -1,5 +1,8 @@
-# CREATE HOSTNAME
-sudo hostname auto
+# date: sep/12/2022
+# Installing Jenkins on RHEL 7/8, CentOS 7/8 or Amazon Linux os
+# you can execute this script a user-data when launching your EC2 VM.
+sudo timedatectl set-timezone Edmonton/Canada
+sudo hostnamectl set-hostname jenkins
 sudo yum install wget -y
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
@@ -18,3 +21,4 @@ sudo systemctl start jenkins
 # You can check the status of the Jenkins service using the command:
 sudo systemctl status jenkins
 sudo su - ec2-user
+echo "end of jenkins installation"
